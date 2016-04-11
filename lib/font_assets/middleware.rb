7d5c63235cf5507b4990d1cbb877b77ffa3ea9d5
akarnani1 <abhishek.karnani224@gmail.com>
2016-04-12 03:16:33 +0530

    master

---
+++ lib/font_assets/middleware.rb
@@ -0,0 +1,90 @@
+require 'rack'
+require 'font_assets/mime_types'
+
+module FontAssets
+  class Middleware
+
+    def initialize(app, origin, options={})
+      @app = app
+      @origin = origin
+      @options = options
+      @mime_types = FontAssets::MimeTypes.new(Rack::Mime::MIME_TYPES)
+    end
+
+    def access_control_headers
+      {
+        "Access-Control-Allow-Origin" => origin,
+        "Access-Control-Allow-Methods" => "GET",
+        "Access-Control-Allow-Headers" => "x-requested-with",
+        "Access-Control-Max-Age" => "3628800"
+      }
+    end
+
+    def call(env)
+      @ssl_request = Rack::Request.new(env).scheme == "https"
+      # intercept the "preflight" request
+      if env["REQUEST_METHOD"] == "OPTIONS"
+        if ext = extension(env["PATH_INFO"]) and font_asset?(ext)
+          return [200, access_control_headers, []]
+        else
+          return @app.call(env)
+        end
+      else
+        path = env["PATH_INFO"].dup
+        code, headers, body = @app.call(env)
+        set_headers! headers, body, path
+        [code, headers, body]
+      end
+    end
+
+
+    private
+
+    def origin
+      if !wildcard_origin? and allow_ssl? and ssl_request?
+        uri = URI(@origin)
+        uri.scheme = "https"
+        uri.to_s
+      else
+        @origin
+      end
+    end
+
+    def wildcard_origin?
+      @origin == '*'
+    end
+
+    def ssl_request?
+      @ssl_request
+    end
+
+    def allow_ssl?
+      @options[:allow_ssl]
+    end
+
+    def extension(path)
+      if path.nil? || path.length == 0
+        nil
+      else
+        "." + path.split("?").first.split(".").last
+      end
+      rescue
+        "."
+    end
+
+    def font_asset?(path)
+      @mime_types.font? extension(path)
+    end
+
+    def set_headers!(headers, body, path)
+      if ext = extension(path) and font_asset?(ext)
+        headers.merge!(access_control_headers)
+        headers.merge!('Content-Type' => mime_type(ext)) if headers['Content-Type']
+      end
+    end
+
+    def mime_type(extension)
+      @mime_types[extension]
+    end
+  end
+end
---
+++ lib/font_assets/railtie.rb
@@ -0,0 +1,23 @@
+require 'font_assets/middleware'
+
+module FontAssets
+  class Railtie < Rails::Railtie
+    config.font_assets = ActiveSupport::OrderedOptions.new
+
+    initializer "font_assets.configure_rails_initialization" do |app|
+      config.font_assets.origin ||= "*"
+      config.font_assets.options ||= { allow_ssl: true }
+
+      config.font_assets.insert_target = if defined?(ActionDispatch::Static)
+        'ActionDispatch::Static'
+      else
+        'Rack::Runtime'
+      end
+
+      app.middleware.insert_before config.font_assets.insert_target,
+        FontAssets::Middleware,
+        config.font_assets.origin,
+        config.font_assets.options
+    end
+  end
+end
